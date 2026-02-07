42899d3c36689165d59c6f0be140caf3c731e72e
MrReedQEGS <60434301+MrReedQEGS@users.noreply.github.com>
2026-02-07 08:34:08 +0000

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -4,3 +4,6 @@ output = document.getElementById("output")
 output.innerHTML = "<b>helloworld.py loaded and running!</b> 🚀"
 
 print("This prints to the PyScript console")
+
+for i in range(1,10):
+  print("Hello : " + str(i))
